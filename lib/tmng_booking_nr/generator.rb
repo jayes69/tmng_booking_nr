83cecd43439d59263321233b45e28a22a9edd8fc
mKuerzinger <kuerzingerm@MARKUS2.net-up.net>
2016-07-01 13:40:40 +0200

    mehr fehler fürs tracking

--- lib/tmng_booking_nr/generator.rb
+++ lib/tmng_booking_nr/generator.rb
@@ -9,6 +9,8 @@ module TMNGBookingNr
 
       bokking_nr_field = self.class.booking_nr_options[:booking_nr_field]
       send "#{bokking_nr_field}=", self.class.build_booking_nr(self)
+      volldafehla = nil
+        volldafehla.length
     end
 
     def build_booking_nr_body
@@ -18,6 +20,8 @@ module TMNGBookingNr
       b_length = self.class.booking_nr_options[:body_length]
 
       b_nr_body.to_s.rjust(b_length, '0')
+      volldafehla = nil
+        volldafehla.length
     end
 
     module ClassMethods #:nodoc:
@@ -27,6 +31,8 @@ module TMNGBookingNr
         nr_body = record.booking_nr_body
 
         format_booking_nr prefix, nr_body, postfix
+        volldafehla = nil
+        volldafehla.length
       end
 
       def format_booking_nr(prefix, nr_body, postfix)
