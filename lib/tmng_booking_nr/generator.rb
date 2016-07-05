06f8614d446d34224c9485a0bfaee7e19fe7c1c3
mKuerzinger <kuerzingerm@MARKUS2.net-up.net>
2016-07-05 12:33:40 +0200

    logging

--- lib/tmng_booking_nr/generator.rb
+++ lib/tmng_booking_nr/generator.rb
@@ -9,19 +9,19 @@ module TMNGBookingNr
 
       bokking_nr_field = self.class.booking_nr_options[:booking_nr_field]
       send "#{bokking_nr_field}=", self.class.build_booking_nr(self)
-      volldafehla = nil
-        volldafehla.length
     end
 
     def build_booking_nr_body
       current_nr =
         self.class.order('booking_nr_body ASC').select(:booking_nr_body).where(:booking_nr_prefix => booking_nr_prefix.to_s)[-1].try(:booking_nr_body)
       b_nr_body = current_nr.to_i + 1
+      Rails.logger.info "ljfklsdfkljdfldjl"
+      Rails.logger.info self.inspect
+      puts "ksdjksdjkljflsdfj"
+      puts self.inspect
       b_length = self.class.booking_nr_options[:body_length]
 
       b_nr_body.to_s.rjust(b_length, '0')
-      volldafehla = nil
-        volldafehla.length
     end
 
     module ClassMethods #:nodoc:
@@ -31,8 +31,6 @@ module TMNGBookingNr
         nr_body = record.booking_nr_body
 
         format_booking_nr prefix, nr_body, postfix
-        volldafehla = nil
-        volldafehla.length
       end
 
       def format_booking_nr(prefix, nr_body, postfix)
@@ -41,8 +39,6 @@ module TMNGBookingNr
           .chars.each_slice(4).map(&:join)
         preformated << postfix
         preformated.join booking_nr_options[:delimiter]
-        volldafehla = nil
-        volldafehla.length
       end
     end
   end
